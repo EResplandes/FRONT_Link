7193097485a49da0fcfa3a8691d650a3522242c5
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-06-05 08:16:32 -0300

    Correcao tela Dr. Mônica

--- src/views/pages/presidencia/Monica.vue
+++ src/views/pages/presidencia/Monica.vue
@@ -37,7 +37,7 @@ export default {
             titleChat: '',
             salvarMensagemPedidoStatus: ref(null),
             pedidoSelecionado: {},
-            heightVH: (window.innerHeight * 0.71)
+            heightVH: window.innerHeight * 0.71
         };
     },
 
@@ -50,7 +50,6 @@ export default {
             };
         }
     },
-
 
     mounted: function () {
         if (window.AdobeDC) {
@@ -225,6 +224,15 @@ export default {
             });
         },
 
+        buscaPedidos() {
+            // Metódo responsável por buscar quantidades de pedidos para aprovação
+            this.pedidoService.pedidosMonica().then((data) => {
+                console.log(data);
+                this.pedidos = data.pedidos;
+                this.preloading = false;
+            });
+        },
+
         // Metódo responsávle por buscar proximo pedidoAcima
         proximoItemAcima() {
             // Verifica se currentIndex é igual ao comprimento dos pedidos
@@ -247,8 +255,8 @@ export default {
                         this.visualizarAcima(this.pedidos[this.currentIndex].id, this.pedidos[this.currentIndex]);
                         localStorage.setItem('ultimoPedidoAprovado', this.currentIndex);
                     } else {
-                        this.listarEmivalMaiorMil();
-                        this.displayAcima = false;
+                        this.display = false;
+                        this.buscaPedidos();
                     }
                 });
             } else {
@@ -556,48 +564,6 @@ export default {
                 </div>
             </div>
         </Dialog>
-        <!-- Anexo -->
-        <Dialog header="Insira novo Anexo" v-model:visible="displayAnexo" :style="{ width: '30%' }" :modal="true">
-            <div class="grid mt-5 text-center flex justify-content-center align-items-center">
-                <FileUpload uploadLabel="Salvar" cancelLabel="Limpar PDF" chooseLabel="Selecione" @change="uploadPdf" type="file" ref="pdf" name="demo[]" accept=".pdf,.docx" :maxFileSize="1000000"></FileUpload>
-            </div>
-            <div>
-                <Button @click.prevent="this.displayAnexo = false" label="Salvar" class="mr-2 mt-3 p-button-success col-12" />
-            </div>
-        </Dialog>
-
-        <!-- Modal Filtros -->
-        <Sidebar style="width: 500px" v-model:visible="visibleRight" :baseZIndex="1000" position="right">
-            <h3 v-if="this.editar == false" class="titleForm">Filtros</h3>
-
-            <div class="card p-fluid">
-                <div class="field">
-                    <label for="empresa">Empresa:</label>
-                    <Dropdown v-model="form.empresa" :options="empresas" showClear optionLabel="nome_empresa" placeholder="Selecione..." class="w-full" />
-                </div>
-                <div class="field">
-                    <label for="empresa">Status:</label>
-                    <Dropdown v-model="form.status" :options="status" showClear optionLabel="status" placeholder="Selecione..." class="w-full" />
-                </div>
-                <div class="field">
-                    <label for="cpf">Descrição: </label>
-                    <InputText v-tooltip.left="'Digite a descrição do pedido'" v-model="form.descricao" id="cnpj" placeholder="Digite..." />
-                </div>
-                <div class="field">
-                    <label for="cpf">Valor: </label>
-                    <InputNumber v-tooltip.left="'Digite o valor do pedido'" v-model="form.valor" inputId="minmaxfraction" :minFractionDigits="2" :maxFractionDigits="2" placeholder="Digite..." />
-                </div>
-                <div class="field">
-                    <label for="cpf">Dt. In clusão:</label>
-                    <Calendar dateFormat="dd/mm/yy" v-tooltip.left="'Selecione a data de inclusão'" v-model="form.dt_inclusao" showIcon :showOnFocus="false" class="" />
-                </div>
-                <hr />
-                <div class="field">
-                    <Button @click.prevent="buscaFiltros()" label="Filtrar" class="mr-2 mb-2 p-button-secondary" />
-                    <Button @click.prevent="limparFiltro()" label="Limpar Filtros" class="mr-2 mb-2 p-button-danger" />
-                </div>
-            </div>
-        </Sidebar>
 
         <!-- Tabela com todos pedidos -->
         <div class="col-12">
